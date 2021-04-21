df174e9c7a3c6315d6cc6d6625bc44eb492a7c86
Alexander <allemeshov@gmail.com>
2021-04-21 03:48:57 +0300

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import { Environment } from "src/app/shared/interfaces/environment.interface";
+
+export const environment: Environment = {
+  apiUrl: 'http://akiana.io/api',
+  production: false
+};
